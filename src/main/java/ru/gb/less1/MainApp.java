ecffdd8b0c28426ca8e9a2aa330a48e35669f2f8
gavrbr <gavrbr@gmail.com>
2021-07-27 15:04:22 +0300

    FirstStep

---
+++ src/main/java/ru/gb/less1/MainApp.java
@@ -0,0 +1,15 @@
+package ru.gb.less1;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("           _..---...,\"\"-._     ,/}/)\n" +
+                "        .''        ,      ``..'(/-<\n" +
+                "       /   _      {      )         \\\n" +
+                "      ;   _ `.     `.   <         a(\n" +
+                "    ,'   ( \\  )      `.  \\ __.._ .: y\n" +
+                "   (  <\\_-) )'-.____...\\  `._   //-'\n" +
+                "    `. `-' /-._)))      `-._)))\n" +
+                "      `...'         ");
+    }
+
+k}
